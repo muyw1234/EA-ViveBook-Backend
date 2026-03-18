d8d9020ec3e3ee9d45b59e69fe34b956e06f32cf
Wenjie Chen <wenjie.sab@gmail.com>
2026-03-18 21:22:01 +0100

    soft delete readded in Usuario and added in Post

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -11,6 +11,7 @@ export interface IPost extends Document {
     description: string;
     status: string; //PostStatus; // nunca he probado a hacer un enum en typescript
     imageUrl?: string; // opcional, si no sube nada entonces le ponemos un imagen default
+    IsDeleted?: boolean;
     ownerId: mongoose.Types.ObjectId | string;
     bookId: mongoose.Types.ObjectId | string;
 }
@@ -20,6 +21,7 @@ const PostSchema = new Schema(
         description: { type: String, default: '' },
         status: { type: String, required: true, enum: ['VENTA', 'ALQUILER', 'NO_DISPONIBLE'] },
         imageUrl: { type: String },
+        IsDeleted: { type: Boolean, default: false },
         ownerId: { type: Schema.Types.ObjectId, required: true, ref: 'Usuario' },
         bookId: { type: Schema.Types.ObjectId, required: true, ref: 'Libro' }
     },
--- src/models/Usuario.ts
+++ src/models/Usuario.ts
@@ -4,15 +4,17 @@ export interface IUsuario {
     name: string;
     email: string;
     password: string;
+    IsDeleted?: boolean;
 }
 
-export interface IUsuarioModel extends IUsuario, Document { }
+export interface IUsuarioModel extends IUsuario, Document {}
 
 const UsuarioSchema: Schema = new Schema(
     {
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
-        password: { type: String, required: true }
+        password: { type: String, required: true },
+        IsDeleted: { type: Boolean, default: false }
     },
     {
         timestamps: true,
