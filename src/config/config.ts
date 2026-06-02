f9dd27a8f81a67be22f9ae2df02ab2696886087a
Wenjie Chen <wenjie.sab@gmail.com>
2026-06-02 12:37:56 +0100

    Ejercicio resuelto

--- src/config/config.ts
+++ src/config/config.ts
@@ -14,6 +14,8 @@ const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '7d';
 const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || 'Pon el tuyo';
 const CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET || 'Pon el turo';
 const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || 'Pon el tuyo';
+const MATOMO_INSTANCE = process.env.MATOMO_INSTANCE || 'https://your-matomo-instance.com';
+const MATOMO_API = process.env.MATOMO_API || 'your-api-key';
 
 export const config = {
     mongo: {
@@ -34,5 +36,9 @@ export const config = {
         apiKey: CLOUDINARY_API_KEY,
         secret: CLOUDINARY_SECRET,
         name: CLOUDINARY_NAME
+    },
+    matomo: {
+        instance: MATOMO_INSTANCE,
+        api: MATOMO_API
     }
 };
--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ import chatRoutes from './routes/Chat';
 import mensajeRoutes from './routes/Mensaje';
 import authRoutes from './routes/auth';
 import postRoutes from './routes/Post';
+import matomoRoute from './routes/Matomo';
 import valoracionRoutes from './routes/Valoracion';
 import imageRoutes from './routes/Image';
 import swaggerUi from 'swagger-ui-express';
@@ -60,6 +61,7 @@ const StartServer = () => {
     router.use('/posts', postRoutes);
     router.use('/valoraciones', valoracionRoutes);
     router.use('/image', imageRoutes);
+    router.use('/matomo', matomoRoute);
 
     /** Healthcheck */
     router.get('/ping', (req, res, next) => res.status(200).json({ hello: 'world' }));
